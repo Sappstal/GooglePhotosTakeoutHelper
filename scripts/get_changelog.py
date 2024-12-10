491c6feda75ea3cabfb17e62aa753e125f1d3e4b
Hellopriyasingh <vah3124@outlook.com>
2024-12-09 17:48:47 -0800

    DWQW

--- scripts/get_changelog.py
+++ scripts/get_changelog.py
@@ -1,21 +1,4 @@
-import argparse
-import re
-
-p = argparse.ArgumentParser()
-p.add_argument('--version', help='Version to get changelog for. You can type with or without "v" prefix')
-args = p.parse_args()
-
-version = args.version.strip().replace('v', '')
-if not re.match(r'\d+.\d+.\d+', version):
-    raise ValueError('Invalid version')
-
-with open("CHANGELOG.md", 'r') as f:
-    lines = f.read()
-
-# Get first "##" followed by version
-start = lines.index(f'## {version}')
-# Start from newline
-start = lines.index('\n', start) + 1
+at
 # Find next "##" (previous version)
 try:
     end = lines.index('\n## ', start)
